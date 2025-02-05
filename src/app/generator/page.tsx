c8c1c145cfe775fd7411598006fa42fad261e9cf
Abderrahmen Mhemed <mhemedabderrahmen@gmail.com>
2025-02-05 09:53:51 +0000

    feat: add shadcn and update readme for tasks

---
+++ src/app/generator/page.tsx
@@ -0,0 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { HydrateClient } from "@/trpc/server";
+import Link from "next/link";
+
+export default async function Generator() {
+  return (
+    <HydrateClient>
+      <main className="flex min-h-screen w-screen flex-col">
+        <div className="container flex flex-col gap-8 px-4 py-4">
+          <div className="flex flex-row items-center justify-between">
+            <h1 className="text-xl font-extrabold tracking-tight sm:text-[5rem]">
+              <Link href={"/"}>
+                Link <span className="text-blue-500">Boost</span>
+              </Link>
+            </h1>
+            <div>
+              <Button size={"sm"} variant={"ghost"}>
+                Docs
+              </Button>
+            </div>
+          </div>
+          <div>
+            <p>
+              Elit officia voluptate in id reprehenderit. Sit irure ut cupidatat
+              irure cillum cillum qui est et. Nisi laborum ullamco occaecat
+              laboris aute adipisicing ullamco incididunt. Cupidatat aliqua
+              cillum id tempor ex est eu in ex minim. Duis nostrud incididunt
+              veniam eu laborum cupidatat nisi aliquip cillum pariatur.
+            </p>
+          </div>
+        </div>
+      </main>
+    </HydrateClient>
+  );
+}
